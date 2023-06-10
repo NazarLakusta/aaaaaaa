f97c13ba90b46608e3831bca9ad956fa23523f77
NazarLakusta <lakusta.nazar.clg@chnu.edu.ua>
2023-06-10 11:09:18 +0300

    Hi Nazar

--- src/Solution.java
+++ src/Solution.java
@@ -2,5 +2,9 @@ public class Solution {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hi Nazar");
+        System.out.println("Hi Nazar");
+        System.out.println("Hi Nazar");
+        System.out.println("Hi Nazar");
+        System.out.println("Hi Nazar");
     }
 }
